fcf3a671165c43e7959fbefbe8c2faf8da3eb9a2
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-10-11 13:48:36 +0200

    protect redis call

--- pybossa/feed.py
+++ pybossa/feed.py
@@ -17,6 +17,8 @@
 # along with PYBOSSA.  If not, see <http://www.gnu.org/licenses/>.
 import json
 from time import time
+import os
+
 from pybossa.core import sentinel
 try:
     import cPickle as pickle
@@ -26,21 +28,25 @@ except ImportError:  # pragma: no cover
 
 FEED_KEY = 'pybossa_feed'
 
+
 def update_feed(obj):
     """Add domain object to update feed in Redis."""
-    pipeline = sentinel.master.pipeline()
-    serialized_object = pickle.dumps(obj)
-    pipeline.zadd(FEED_KEY, time(), serialized_object)
-    pipeline.execute()
+    if os.environ.get('PYBOSSA_REDIS_CACHE_DISABLED') is None:
+        pipeline = sentinel.master.pipeline()
+        serialized_object = pickle.dumps(obj)
+        pipeline.zadd(FEED_KEY, time(), serialized_object)
+        pipeline.execute()
+
 
 def get_update_feed():
     """Return update feed list."""
-    data = sentinel.slave.zrevrange(FEED_KEY, 0, 99, withscores=True)
     feed = []
-    for u in data:
-        tmp = pickle.loads(u[0])
-        tmp['updated'] = u[1]
-        if tmp.get('info') and type(tmp.get('info')) == unicode:
-            tmp['info'] = json.loads(tmp['info'])
-        feed.append(tmp)
+    if os.environ.get('PYBOSSA_REDIS_CACHE_DISABLED') is None:
+        data = sentinel.slave.zrevrange(FEED_KEY, 0, 99, withscores=True)
+        for u in data:
+            tmp = pickle.loads(u[0])
+            tmp['updated'] = u[1]
+            if tmp.get('info') and type(tmp.get('info')) == unicode:
+                tmp['info'] = json.loads(tmp['info'])
+            feed.append(tmp)
     return feed
