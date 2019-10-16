6f31a344bc0dbee75f1d0b98e4458274592ffe37
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-10-16 11:24:01 +0200

    add ReverseProxied

--- run.py
+++ run.py
@@ -37,6 +37,30 @@ class PrefixMiddleware(object):
             return ["This url does not belong to the app.".encode()]
 
 
+class ReverseProxied(object):
+
+    def __init__(self, app, script_name=None, scheme=None, server=None):
+        self.app = app
+        self.script_name = script_name
+        self.scheme = scheme
+        self.server = server
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '') or self.script_name
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+        scheme = environ.get('HTTP_X_SCHEME', '') or self.scheme
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        server = environ.get('HTTP_X_FORWARDED_SERVER', '') or self.server
+        if server:
+            environ['HTTP_HOST'] = server
+        return self.app(environ, start_response)
+
+
 if __name__ == "__main__":  # pragma: no cover
     app = create_app()
     #logging.basicConfig(level=logging.NOTSET)
@@ -46,7 +70,8 @@ if __name__ == "__main__":  # pragma: no cover
         from gevent.pywsgi import WSGIServer
         port = app.config['PORT']
         if app.config.get('URL_PREFIX'):
-            app.wsgi_app = PrefixMiddleware(app.wsgi_app, app.config['URL_PREFIX'])
+            app.wsgi_app = ReverseProxied(app.wsgi_app, script_name=app.config['URL_PREFIX'])
+            # app.wsgi_app = PrefixMiddleware(app.wsgi_app, app.config['URL_PREFIX'])
         WSGIServer(('0.0.0.0', port), app).serve_forever()
     else:
         # run in debug mode
